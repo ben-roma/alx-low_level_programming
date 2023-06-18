1fa3ab84a5d80f7aedf5bdac844f4c8641857e9a
root <root@mail.agrica.ulk>
2023-06-18 10:20:50 +0200

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
- /* output with puts command */
+ /*
+  *  output with puts command
+ */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
