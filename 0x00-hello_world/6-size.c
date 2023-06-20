bbcf4970cd89c2991ef6c114a6dcc95a657b39f8
ben-roma <obameben@yahoo.fr>
2023-06-21 00:54:21 +0200

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry poiny for the progarmme
- *
- * return 0 if there isn't error 
- */
+ *main - Entry poiny for the progarmme
+ *return 0 if there isn't error
+*/
 
 int main(void)
 {
@@ -13,6 +12,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
-	
