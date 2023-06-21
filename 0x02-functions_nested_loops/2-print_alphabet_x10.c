080caefbe664fb6c05a5a04ad7959e6d9691fff3
ben-roma <obameben@yahoo.fr>
2023-06-21 13:33:49 +0200

    update 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+**/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char alpha;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+		_putchart(alpha);
+	}
+}
