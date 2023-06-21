a1976a5e13e56ccf033bddf324b1ff36a5ed2db0
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-06-21 10:46:03 +0200

    0-putchar.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib>
+#include <time.h>
+
+int main(void)
+{
+    int n;
+    srand(time(0));
+    n = rand() - RAND-MAX / 2;
+    if(n>0)
+    {
+        printf
+    }
+
+}
