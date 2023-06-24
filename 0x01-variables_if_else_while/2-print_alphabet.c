9751170af62681d08e2db81f85c2da9be74e60ac
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-06-24 12:31:35 +0200

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,11 @@
 int main(void)
 
 {
-	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 97; i <=122; i++)
 	{
-		putchar(alpha[i]);
+		putchar(i);
 	}
 	putchar('\n');
 
