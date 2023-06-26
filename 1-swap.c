e294adc3253bc16095331c938c02bea8b7585cc1
ben-roma <obameben@yahoo.fr>
2023-06-27 00:40:02 +0200

    update 1-swap.c

---
+++ 1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swaps the values of two integers.
+ * @a: first integer to swap
+ * @b: second integer to swap
+ * Return: no return
+**/
+
+void void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *b;
+	*b = *a;
+	*a = c;
+}
