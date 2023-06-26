c24d71e72478672f127fe8cbfa2509df68d3ce1d
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-06-27 01:55:21 +0200

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,11 +17,11 @@ void puts_half(char *str)
 	}
 
 	if (count % 2 == 1)
-		i = (count / 2) + 1;
+		i = count / 2;
 	else
-		i = ((count - 1) / 2) + 1;
+		i = (count - 1) / 2;
 
-	for (i; i < count; i++)
+	for (i = i + 1; i < count; i++)
 		_putchar(str[i]);
 	_putchar('\n');
 }
