f850bddd6c27715814cf2651941edfd0cd962b91
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-07-18 04:33:57 +0200

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,17 +3,23 @@
 
 /**
  * struct dog - A new type describing a dog.
- * dog_t - Typedef for strut dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
  */
-typedef struct dog dog_t;
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+* dog_t - Typedef for struct dog
+*/
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
