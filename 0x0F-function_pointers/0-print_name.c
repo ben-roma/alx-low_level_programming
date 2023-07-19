d568121df642b4926c7bf58dc89860d1a503606a
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-07-19 19:53:08 +0200

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
  * print_name - prints a name.
  * @name: input name.
  * @f: function pointer
- * 
+ *
  * Return: void
 */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name || f)
-    f(name);
+	if (name || f)
+		f(name);
 }
