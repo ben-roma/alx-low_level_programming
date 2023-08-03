675ea5cdf67637942e5915670a8260c3e01e6691
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-03 18:25:40 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,30 +7,32 @@
  *
  * Return: unsigned int.
  **/
+
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
 
-    if (b == NULL)
-    {
-        return (0); /* return 0 */
-    }
+	if (b == NULL)
+	{
+	return (0); /* return 0 */
+	}
 
-    while (*b)
-    {
-        if (*b == '1')
-        {
-            result = (result << 1) | 1;
-        }
-        else if (*b == '0')
-        {
-            result = result << 1;
-        }
-        else
-        {
-            return (0);
-        }
-        b++;
-    }
-    return result;
+	while (*b)
+	{
+	if (*b == '1')
+	{
+		result = (result << 1) | 1;
+	}
+	else if (*b == '0')
+	{
+		result = result << 1;
+	}
+	else
+	{
+		return (0);
+	}
+	b++;
+	}
+	return (result);
 }
+
