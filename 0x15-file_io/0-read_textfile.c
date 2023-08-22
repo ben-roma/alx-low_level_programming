5008e634b9a39de658adfd438e065e17727e8094
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-22 18:25:03 +0200

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,36 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * read_textfile - Reads a file and prints it to the POSIX stdout.
- * @filename: The name of the file that needs to be read.
- * @letters: Is the number of letters the function should print.
- * Return: The actual number of letters it could read and print.
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int rd;
-	int i;
-	int reading;
+	int fd;
+	ssize_t nrd, nwr;
 	char *buf;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-		return (0);
-	rd = open(filename, O_RDONLY);
-	if (rd == -1)
-	{
-		free(buf);
-		return (0);
-	}
 
-	reading = read(rd, buf, letters);
-	if (reading == -1)
-	{
-		close(rd);
-		free(buf);
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
 		return (0);
-	}
 
-	for (i = 0; i < reading; i++)
-	{
-		if (write(STDOUT_FILENO, &buf[i], 1) == -1)
-		{
-			close(rd);
-			free(buf);
-			return (0);
-		}
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
 
-	}
-	close(rd);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
 	free(buf);
-	return (reading);
+
+	return (nwr);
 }
