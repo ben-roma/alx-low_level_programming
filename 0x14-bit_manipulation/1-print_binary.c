c632a8e0fa06589ffbe77cfd505a84f93e808079
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-22 18:23:21 +0200

    Update 1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,28 +1,22 @@
-#include "holberton.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * print_binary - prints the binary representation of a number
- * @n: number to be represented
- * Return: nothing
- **/
+ * print_binary - prints the binary representation
+ * of a number.
+ * @n: unsigned long int.
+ *
+ * Return: no return.
+ */
 void print_binary(unsigned long int n)
 {
-	char i;
-
-	if (n == 0)
+	if (n >> 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
 	{
 		_putchar('0');
-		return;
 	}
-	if (n == 1)
-	{
-		_putchar('1');
-		return;
-	}
-	print_binary(n >> 1);
-	i = (n & 1) ? '1' : '0';
-	_putchar(i);
 }
