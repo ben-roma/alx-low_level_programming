67210264629d97a56d89271a2d77e978d525f331
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-08-22 18:23:15 +0200

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,35 +1,37 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * binary_to_uint - Converters a binary number to an unsigned integer
- * @b: String representing a binary number
- * Return: The converted number or 0 if b is NULL or if there are chars
- * that are not 0 or 1
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, sum, pow;
-	int base;
+	unsigned int ui;
+	int len, base_two;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-
-	if (b == NULL)
+	if (!b)
 		return (0);
+
+	ui = 0;
+
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return ((b[0] - 48));
-	for (i = 0; b[i] != '\0'; i++)
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != '0' && b[len] != '1')
+		{
 			return (0);
-		for (j = len - 1; j > 0; j--)
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
-		len--;
-		pow = 1;
+		}
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (sum);
+
+	return (ui);
 }
