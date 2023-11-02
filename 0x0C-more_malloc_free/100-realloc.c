eba5c9e31f0ab6c3a4759e554d326fcb84ba2d29
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-11-02 20:52:10 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,34 +13,35 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
+	void *new_ptr;
 
-    /* If new_size is equal to old_size, return the original ptr */
-    if (new_size == old_size)
-        return (ptr);
+	/* If new_size is equal to old_size, return the original ptr */
+	if (new_size == old_size)
+		return (ptr);
 
-    /* If new_size is 0 and ptr is not NULL, free ptr and return NULL */
-    if (new_size == 0 && ptr != NULL)
-    {
-        free(ptr);
-        return (NULL);
-    }
+	/* If new_size is 0 and ptr is not NULL, free ptr and return NULL */
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-    /* If ptr is NULL, the behavior is the same as calling malloc(new_size) */
-    if (!ptr)
-        return (malloc(new_size));
+	/* If ptr is NULL, the behavior is the same as calling malloc(new_size) */
+	if (!ptr)
+		return (malloc(new_size));
 
-    /* Allocate memory for the new block */
-    new_ptr = malloc(new_size);
-    if (!new_ptr)
-        return (NULL);
+	/* Allocate memory for the new block */
+	new_ptr = malloc(new_size);
+	if (!new_ptr)
+		return (NULL);
 
     /* Copy the content from old block to new block */
-    if (new_size > old_size)
-        memcpy(new_ptr, ptr, old_size);
-    else
-        memcpy(new_ptr, ptr, new_size);
+	if (new_size > old_size)
+		memcpy(new_ptr, ptr, old_size);
+	else
+		memcpy(new_ptr, ptr, new_size);
 
-    free(ptr); /* Free the old block */
-    return (new_ptr);
+	free(ptr); /* Free the old block */
+	return (new_ptr);
 }
+
