3652fd80b5fab3e1fbfdd0988117a17d220dd26f
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-11-02 20:15:51 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,43 +1,47 @@
-#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
  * string_nconcat - concatenates two strings
- * @s1: first string to join
- * @s2: string to join
- * @n: no
- * Return: NULL or pointer
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * @n: maximum number of bytes from s2 to concatenate to s1
+ *
+ * Return: pointer to the newly allocated space in memory
  */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, len1 = 0, len2 = 0;
-	char *p;
+	unsigned int len1 = 0, len2 = 0;
+	char *new_str;
+	unsigned int i, j;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 	if (n > len2)
 		n = len2;
-	p = malloc(len1 + n + 1);
-	if (p == NULL)
+
+	new_str = (char *)malloc(len1 + n + 1);
+	if (new_str == NULL)
 		return (NULL);
-	for (i = 0; i < len1 + n; i++)
+
+	for (i = 0; i < len1; i++)
 	{
-		if (*s1)
-		{
-			p[i] = *s1;
-			s1++;
-		}
-		else
-		{
-			p[i] = *s2;
-			s2++;
-		}
+		new_str[i] = s1[i];
 	}
-	p[i] = '\0';
-	return (p);
+
+	for (j = 0; j < n; j++, i++)
+	{
+		new_str[i] = s2[j];
+	}
+
+	new_str[i] = '\0';
+
+	return (new_str);
 }
+
