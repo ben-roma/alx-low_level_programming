60d1ca844a9bce8b003d746c7e5e48a482935f6d
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-11-06 11:49:14 +0100

    Update 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -16,18 +16,18 @@ int main(void)
 
 	while (diff > 126) /* Tant que la différence est plus grande que le caractère ASCII maximal */
 	{
-		c = (rand() % (126 - 32 + 1)) + 32; /*Génère un caractère imprimable*/ 
-		sum += c; // Ajoute la valeur ASCII du caractère à la somme
+		c = (rand() % (126 - 32 + 1)) + 32; /* Génère un caractère imprimable */
+		sum += c; /* Ajoute la valeur ASCII du caractère à la somme */
 		if (sum > 2772)
 		{
-			sum -= c; /* Enlève le dernier caractère si la somme dépasse 2772*/
+			sum -= c; /* Enlève le dernier caractère si la somme dépasse 2772 */
 			continue;
 		}
-		diff -= c; /* Met à jour la différence requise*/
+		diff -= c; /* Met à jour la différence requise */
 		printf("%c", c); /* Affiche le caractère */
 	}
 
-	/* Imprime le dernier caractère pour atteindre exactement la somme de 2772*/
+	/* Imprime le dernier caractère pour atteindre exactement la somme de 2772 */
 	printf("%c\n", diff); /* Assure que le dernier caractère fait atteindre la somme exacte de 2772 */
 
 	return (0);
