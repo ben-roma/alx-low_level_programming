37aeec41212fdf3c63adb664534dc715e9c7459f
Ben Obame <44915209+ben-roma@users.noreply.github.com>
2023-11-08 16:25:52 +0100

    Update 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+    int num1, num2;
+    char *operator;
+    int (*op_func)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = atoi(argv[1]);
+    operator = argv[2];
+    num2 = atoi(argv[3]);
+
+    op_func = get_op_func(operator);
+    if (!op_func || operator[1] != '\0')
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    printf("%d\n", op_func(num1, num2));
+    return (0);
+}
