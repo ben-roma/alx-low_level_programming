795b70c015f4ec73feeee32d30fd501465eb4cb7
Ben Obame <obameben@yahoo.fr>
2023-11-17 03:25:46 +0200

    singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t node_count;
 
 	nelem = 0;
 	while (h != NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		node_count++;
 	}
-	return (nelem);
+	return (node_count);
 }
