80abfa97cff8947dcdc8fc4cc2713862db1819f4
Ben Obame <obameben@yahoo.fr>
2023-11-27 19:17:22 +0200

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * create_file - function; this creates a file
  *
@@ -10,31 +15,35 @@
  * Return: gives a (-ve) -1 when function fails
  *         Elsewhere returns - 1.
  */
+
+
+
 int create_file(const char *filename, char *text_content)
 {
-	int fd, write_result, length = 0;
+	/***sets constants***/
+	int f_d;
+	ssize_t len = 0;
+	ssize_t b_w;
 
 	if (filename == NULL)
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	f_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f_d == -1)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		while (text_content[length])
-		length++;
+		while (text_content[len] != '\0')
+			len++;
+		b_w = write(f_d, text_content, len);
+		close(f_d);
 
-		write_result = write(fd, text_content, length);
-		if (write_result == -1)
-		{
-			close(fd);
+		if (b_w == -1)
 			return (-1);
-		}
 	}
-
-	close(fd);
+	else
+	{
+		close(f_d);
+	}
 	return (1);
 }
 
